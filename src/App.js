28c60ac467380649b5d12605e863b66c041d196b
zorianafoshchan <zoryana0403@gmail.com>
2024-02-09 17:17:25 +0200

    posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, {useState} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+ const [posts, setPosts] = useState([
+  {
+   id: 1,
+   title: "My first Post",
+   datetime: "July 02, 2023 12:36:25 AM",
+   body: "",
+  },
+  {
+   id: 2,
+   title: "My second Post",
+   datetime: "July 04, 2023 15:36:55 AM",
+   body: "Some text"
+  },
+  {
+   id: 3,
+   title: "My third Post",
+   datetime: "July 08, 2023 10:36:25 AM",
+   body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe aspernatur culpa explicabo obcaecati in cupiditate"
+  },
+  {
+   id: 4,
+   title: "My fourth Post",
+   datetime: "July 22, 2023 22:22:22 AM",
+   body: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe aspernatur culpa explicabo obcaecati in cupiditate"
+  },
+
+])
+
   return (
     <div className="App">
       <header className="App-header">
